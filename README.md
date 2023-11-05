dbf9e60046d8d924d5c9c02f9bd6f0156653de43
Kevin <lexdxw@126.com>
2023-11-05 13:55:20 +0800

    Eyes GUI(In developing).

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Shiny-lamp
+
+Shiny-lamp is a beacon on the road to success.
---
+++ Src/EyesGUI/GUI.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyesGUI
+{
+    public class GUI
+    {
+        public void Draw()
+        {
+
+        }
+
+        public GUI(SpriteBatch spriteBatch) 
+        { 
+        }
+    }
+}
